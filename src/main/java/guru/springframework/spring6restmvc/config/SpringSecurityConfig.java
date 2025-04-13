8f30ac637614d45b59d84d42e53af9dad2d27eb1
Juni2799 <akshaankhanna@gmail.com>
2025-04-13 21:33:29 +0530

    Added pom and SecurityConfig changes to generate OpenAPI documentation

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
@@ -10,7 +10,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        http.authorizeHttpRequests(authorize -> authorize
+                        // Skipping authentication validation for Swagger links alone (v3 is default from the dependency)
+                        // ********* Only "http://localhost:8081/swagger-ui/index.html" is working as of now, need to check further *********
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/webjars/**"
+                        ).permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf
                         .ignoringRequestMatchers("/api/**") // Ignore CSRF protection for /api/** endpoints
