fc9c492deba7fcdf5414a00acd11be36284f7fce
Juni2799 <akshaankhanna@gmail.com>
2025-04-14 08:22:32 +0530

    Added changes for Spring Actuator

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
@@ -1,14 +1,28 @@
 package guru.springframework.spring6restmvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    //This bean should be created first in Spring context as the Spring Actuator needs to get
+    //the permission to bypass Spring Security for it's endpoint - http://localhost:8081/actuator/health
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorise -> authorise.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                         // Skipping authentication validation for Swagger links alone (v3 is default from the dependency)
