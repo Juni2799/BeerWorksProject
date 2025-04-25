2244c16dbbd44ac8418455bf96d633521d3fd6fe
Juni2799 <akshaankhanna@gmail.com>
2025-04-25 16:46:58 +0530

    Added initial setup for Audit events

--- src/main/java/guru/springframework/spring6restmvc/listeners/BeerCreatedListener.java
+++ src/main/java/guru/springframework/spring6restmvc/listeners/BeerCreatedListener.java
@@ -1,15 +1,32 @@
 package guru.springframework.spring6restmvc.listeners;
 
 import guru.springframework.spring6restmvc.events.BeerCreatedEvent;
+import guru.springframework.spring6restmvc.mappers.BeerMapper;
+import guru.springframework.spring6restmvc.repository.BeerAuditRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class BeerCreatedListener {
+    private final BeerAuditRepository beerAuditRepository;
+    private final BeerMapper beerMapper;
 
     @EventListener
     public void listen(BeerCreatedEvent event){
-        System.out.println("I heard a beer was created!!");
-        System.out.println(event.getBeer().getId());
+        //Create the audit entry
+        val beerAudit = beerMapper.beerToBeerAudit(event.getBeer());
+        beerAudit.setAuditEventType("BEER_CREATED");
+
+        if(event.getAuthentication() != null && event.getAuthentication().getName() != null){
+            beerAudit.setPrincipalName(event.getAuthentication().getName());
+        }
+
+        val savedBeerAudit = beerAuditRepository.save(beerAudit);
+        log.debug("Beer Audit Saved: " + savedBeerAudit.getId());
     }
 }
--- src/main/java/guru/springframework/spring6restmvc/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/spring6restmvc/mappers/BeerMapper.java
@@ -1,12 +1,22 @@
 package guru.springframework.spring6restmvc.mappers;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.BeerAudit;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface BeerMapper {
+    @Mapping(target = "categories", ignore = true)
+    @Mapping(target = "beerOrderLines", ignore = true)
     Beer beerDTOtoBeer(BeerDTO beerDTO);
 
     BeerDTO beerToBeerDTO(Beer beer);
+
+    @Mapping(target = "createdDateAudit", ignore = true)
+    @Mapping(target = "principalName", ignore = true)
+    @Mapping(target = "auditEventType", ignore = true)
+    @Mapping(target = "auditId", ignore = true)
+    BeerAudit beerToBeerAudit(Beer beer);
 }
